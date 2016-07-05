63ca41ef58ebaee115266af7fbb8d2c0167bcaf7
li-xinyuan <lixinyuan@126.com>
2016-07-05 12:08:06 +0800

    test

---
+++ src/lxy/t/test/MySwap.java
@@ -0,0 +1,19 @@
+package lxy.t.test;
+
+public class MySwap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a = 1, b = 2;
+		System.out.println("before : a=" + a + ",b=" + b);
+		swap(a, b);
+		System.out.println("before : a=" + a + ",b=" + b);
+	}
+
+	public static void swap(Integer a, Integer b) {
+		Integer temp = new Integer(a);
+		b = new Integer(temp);
+		Integer temp2 = new Integer(b);
+		a = new Integer(temp2);
+	}
+}
